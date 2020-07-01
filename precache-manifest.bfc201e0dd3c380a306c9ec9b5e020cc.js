36a6feca5b14a6c1ea2d2cc261fdb215de5981e7
Adeel <adeel@wfgames.co.uk>
2020-07-01 15:01:58 +0100

    Updates

---
+++ precache-manifest.bfc201e0dd3c380a306c9ec9b5e020cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ce9a879dd0534a616a36f35aa8e19a",
+    "url": "/roboAssist/index.html"
+  },
+  {
+    "revision": "f5d699386491c4bc1a5d",
+    "url": "/roboAssist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "99254c7fad8fb3e15885",
+    "url": "/roboAssist/static/js/2.90553777.chunk.js"
+  },
+  {
+    "revision": "c79a5395694a47e32f6e2f32e8a9dcf5",
+    "url": "/roboAssist/static/js/2.90553777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5d699386491c4bc1a5d",
+    "url": "/roboAssist/static/js/main.8a47a162.chunk.js"
+  },
+  {
+    "revision": "8161eaacd9946adbe556",
+    "url": "/roboAssist/static/js/runtime-main.371a9e7f.js"
+  },
+  {
+    "revision": "e88949ccd2a3602c0a6025b758b94499",
+    "url": "/roboAssist/static/media/face-talking.e88949cc.png"
+  },
+  {
+    "revision": "a30a2d17a5265d08499bae773949f0c9",
+    "url": "/roboAssist/static/media/face-waiting.a30a2d17.png"
+  }
+]);
